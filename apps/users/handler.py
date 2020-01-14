d2101b9a5aa672c41901b7330392853106dac796
cheung <zlinxx@foxmail.com>
2020-01-14 12:22:37 +0800

    add register

--- apps/users/handler.py
+++ apps/users/handler.py
@@ -1,12 +1,54 @@
 import json
 from functools import partial
 from random import choice
+from datetime import datetime
 
 
-from apps.users.forms import SmsCodeForm, RegisterForm
+import jwt
+from apps.users.forms import SmsCodeForm, RegisterForm, LoginForm
 from apps.utils.AsyncYunPian import AsyncYunPian
 from ZxForm.handler import RedisHandler
 from apps.users.models import User
+
+
+class LoginHandler(RedisHandler):
+    async def post(self):
+        re_data = {}
+
+        param = self.request.body.decode("utf-8")
+        param = json.loads(param)
+        form = LoginForm.from_json(param)
+        if form.validate():
+            mobile = form.mobile.data
+            password = form.mobile.data
+
+            try:
+                user = await self.application.objects.get(User, mobile=mobile)
+                if not user.password.check_password(password):
+                    self.set_status(400)
+                    re_data["non_field"] = "用户名或密码错误"
+                else:
+                    # 登录成功
+                    # session实际上是服务器随机生成的一段字符串,保存在服务器的
+                    # jwt本质上还是加密技术, userid, user.name
+
+                    # 生成json web token
+                    payload = {
+                        "id": user.id,
+                        "nick_name": user.nick_name,
+                        "exp": datetime.utcnow()
+                    }
+                    token = jwt.encode(payload, self.settings["secret_key"], algorithm="HS256")
+                    re_data["id"] = user.id
+                    if user.nick_name is not None:
+                        re_data["nick_name"] = user.nick_name
+                    else:
+                        re_data["nick_name"] = user.mobile
+                    re_data["token"] = token.decode("utf8")
+            except User.DoesNotExist as e:
+                self.set_status(400)
+                re_data["mobile"] = "用户不存在"
+            await self.finish(re_data)
 
 
 
@@ -43,12 +85,6 @@ class RegisterHandler(RedisHandler):
                 re_data[field] = register_form[field][0]
 
         await self.finish(re_data)
-
-
-
-
-
-
 
 
 class SmsHandler(RedisHandler):
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,8 @@
 from tornado.web import url
-from apps.users.handler import SmsHandler
+from apps.users.handler import SmsHandler, RegisterHandler, LoginHandler
 
 urlpattern = (
     url("/code", SmsHandler),
     url("/register", RegisterHandler),
-    # url("/login", LoginHandler),
+    url("/login", LoginHandler),
 )
